e4a59b5a912ca35a5ec5b74a52469b436146ed21
Olga Usovich <olga.usovich.17@mail.ru>
2023-07-16 20:31:20 +0300

    feat: add main styles

---
+++ frontend/src/ui/color.js
@@ -0,0 +1,14 @@
+export const COLOR = {
+  BLACK: "rgba(0, 0, 0, 1)",
+  BLACK_90: "rgba(0, 0, 0, 0.9)",
+  BLACK_70: "rgba(0, 0, 0, 0.7)",
+  BLACK_63: "rgba(0, 0, 0, 0.63)",
+  BLACK_50: "rgba(0, 0, 0, 0.5)",
+  BLACK_37: "rgba(0, 0, 0, 0.37)",
+  BLACK_31: "rgba(0, 0, 0, 0.31)",
+  WHITE: "rgba(255, 255, 255, 1)",
+  WHITE_75: "rgba(255, 255, 255, 0.75)",
+  WHITE_35: "rgba(255, 255, 255, 0.35)",
+  GREY: "rgba(114, 114, 114, 1)",
+  ORANGE: "rgba(235, 141, 77, 1)",
+}
---
+++ frontend/src/ui/index.js
@@ -0,0 +1,5 @@
+import { COLOR } from "./color";
+import { GlobalStyle } from "./globalStyles";
+import { MEDIA } from "./media";
+
+export { COLOR, MEDIA, GlobalStyle };
